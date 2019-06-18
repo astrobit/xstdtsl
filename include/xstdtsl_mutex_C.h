1db80de61116195dd77c409afe84ba6be8eb2a90
Brian W. Mulligan <astrobit2@yahoo.com>
2019-06-18 17:25:44 -0500

    add EXPORT to C library functions

--- include/xstdtsl_mutex_C.h
+++ include/xstdtsl_mutex_C.h
@@ -1,20 +1,24 @@
 #pragma once
+#ifndef EXPORT
+#define EXPORT
+#endif
 
 typedef void * xstdtsl_rwm_t;
 extern "C"
 {
-	xstdtsl_rwm_t	xstdtsl_rwm_new_read_write_mutex(void);
-	bool	xstdtsl_rwm_is_read_locked(xstdtsl_rwm_t);
-	bool	xstdtsl_rwm_is_write_locked(xstdtsl_rwm_t);
-	bool	xstdtsl_rwm_is_unlocked(xstdtsl_rwm_t);
-	int		xstdtsl_rwm_lock_status(xstdtsl_rwm_t);
-	void	xstdtsl_rwm_read_lock(xstdtsl_rwm_t);
-	void	xstdtsl_rwm_write_lock(xstdtsl_rwm_t);
-	void	xstdtsl_rwm_read_unlock(xstdtsl_rwm_t);
-	void	xstdtsl_rwm_write_unlock(xstdtsl_rwm_t);
-	bool	xstdtsl_rwm_try_read_lock(xstdtsl_rwm_t);
-	bool	xstdtsl_rwm_try_write_lock(xstdtsl_rwm_t);
-	void	xstdtsl_rwm_delete_read_write_mutex(xstdtsl_rwm_t &);
-	void	xstdtsl_rwm_set_status(xstdtsl_rwm_t, int);
+	EXPORT xstdtsl_rwm_t	xstdtsl_rwm_new_read_write_mutex(void);
+	EXPORT bool	xstdtsl_rwm_is_read_locked(xstdtsl_rwm_t);
+	EXPORT bool	xstdtsl_rwm_is_write_locked(xstdtsl_rwm_t);
+	EXPORT bool	xstdtsl_rwm_is_unlocked(xstdtsl_rwm_t);
+	EXPORT int		xstdtsl_rwm_lock_status(xstdtsl_rwm_t);
+	EXPORT void	xstdtsl_rwm_read_lock(xstdtsl_rwm_t);
+	EXPORT void	xstdtsl_rwm_write_lock(xstdtsl_rwm_t);
+	EXPORT void	xstdtsl_rwm_read_unlock(xstdtsl_rwm_t);
+	EXPORT void	xstdtsl_rwm_write_unlock(xstdtsl_rwm_t);
+	EXPORT bool	xstdtsl_rwm_try_read_lock(xstdtsl_rwm_t);
+	EXPORT bool	xstdtsl_rwm_try_write_lock(xstdtsl_rwm_t);
+	EXPORT void	xstdtsl_rwm_delete_read_write_mutex(xstdtsl_rwm_t &);
+	EXPORT void	xstdtsl_rwm_set_status(xstdtsl_rwm_t, int);
 }
 
+#undef EXPORT
