e597ef9956e31f2d42e421d01968ff011c676fcf
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2019-06-26 15:34:31 -0500

    create header file for system

---
+++ include/xstdtsl_system_C.h
@@ -0,0 +1,15 @@
+#pragma once
+#if defined _WIN32 || defined _WIN64
+#define __XSTDTSL_EXPORT __declspec(dllexport)
+#else
+#define __XSTDTSL_EXPORT
+#endif
+
+extern "C"
+{
+	__XSTDTSL_EXPORT size_t get_word_size(void) noexcept;
+	__XSTDTSL_EXPORT size_t get_available_memory(void) noexcept;
+}
+
+
+#undef __XSTDTSL_EXPORT
