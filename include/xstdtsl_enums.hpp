004983fc05930c877cb1a79704f7f09d99281883
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2019-07-15 16:19:14 -0500

    move start_point enums into a separate header and namespace

---
+++ include/xstdtsl_enums.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef __XSTDTSL_ENUMS_HPP
+#define __XSTDTSL_ENUMS_HPP
+
+namespace xstdtsl
+{
+	namespace binary_trees
+	{
+		/// 
+		/// enumeration to describe whether the iteration begins at the beginning or end of the tree
+		///
+		enum class start_point {
+				beginning, ///< iteration will begin at the start of the tree
+				root, ///< iteration will begin at the root of the tree
+				end ///< iteration will begin at the end of the tree
+				};
+	}
+
+	namespace lists
+	{
+		/// 
+		/// enumeration to describe whether the iteration begins at the beginning or end of the list
+		///
+		enum class start_point {
+				beginning, ///< iteration will begin at the start of the list
+				end ///< iteration will begin at the end of the list
+				};
+	}
+}
+#endif //#ifndef __XSTDTSL_ENUMS_HPP
