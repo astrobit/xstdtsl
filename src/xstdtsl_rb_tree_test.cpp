733aaa105d301a91da93e9d42bb0996ed6c54b2a
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2019-07-24 17:12:02 -0500

    fix comment at start of rb tree test to correctly indicate that it is r-b tree

--- src/xstdtsl_rb_tree_test.cpp
+++ src/xstdtsl_rb_tree_test.cpp
@@ -18,7 +18,7 @@ typedef xstdtsl::safe_rb_tree<int>::write_control 	write_ctrl;
 
 int main(int i_nNum_Params, char * i_pParams[])
 {
-	std::cout << "--------------=============== safe_avl_tree tests ===============--------------" << std::endl;
+	std::cout << "--------------=============== safe_rb_tree tests ===============--------------" << std::endl;
 	tests::basic_test_suite<tree,read_iter,read_ctrl,write_ctrl>();
 	tests::rb_balancing_1<tree,read_iter>();
 	tests::rb_balancing_2<tree,read_iter>();
